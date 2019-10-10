5b7ba7ae3fb276abcc93cb96cd14d7955f1f2d7c
tgordienko <tgordienko@larch-networks.com>
2019-10-10 15:55:43 +0300

    2

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const bodyParser = require("body-parser");
-
-const Note = require("./models/note");
-
-app.use(bodyParser.json());
 const cors = require("cors");
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 app.use(cors());
-
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-
-app.use(requestLogger);
 
 let notes = [
   {
@@ -41,92 +28,58 @@ let notes = [
   }
 ];
 
-app.use(express.static("build"));
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.post("/api/notes", (request, response, next) => {
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/notes", (request, response) => {
   const body = request.body;
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date()
-  });
-
-  note
-    .save()
-    .then(savedNote => savedNote.toJSON())
-    .then(savedAndFormattedNote => {
-      response.json(savedAndFormattedNote);
-    })
-    .catch(error => next(error));
-});
-
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes);
-  });
-});
-
-app.get("/api/notes/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note.toJSON());
-      } else {
-        response.status(204).end();
-      }
-    })
-    .catch(error => next(error));
-});
-
-app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing"
+    });
+  }
 
   const note = {
     content: body.content,
-    important: body.important
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote.toJSON());
-    })
-    .catch(error => next(error));
+  notes = notes.concat(note);
+
+  response.json(note);
 });
 
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end();
-    })
-    .catch(error => next(error));
+app.get("/notes", (request, response) => {
+  response.json(notes);
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError" && error.kind == "ObjectId") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+app.get("/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
-  next(error);
-};
+});
 
-app.use(errorHandler);
+app.delete("/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end();
+});
 
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
