258ba41a5bcaf8f6d5516ada106930375c9df5c3
tgordienko <tgordienko@larch-networks.com>
2019-10-10 15:41:36 +0300

    Get ready to Heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 let notes = [
   {
@@ -24,62 +26,60 @@ let notes = [
     date: "2019-05-30T19:20:14.298Z",
     important: true
   }
-]
+];
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+  return maxId + 1;
+};
 
-app.post('/notes', (request, response) => {
-  const body = request.body
+app.post("/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
     return response.status(400).json({
-      error: 'content missing'
-    })
+      error: "content missing"
+    });
   }
 
   const note = {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+    id: generateId()
+  };
 
-  notes = notes.concat(note)
+  notes = notes.concat(note);
 
-  response.json(note)
-})
+  response.json(note);
+});
 
-app.get('/notes', (request, response) => {
-  response.json(notes)
-})
+app.get("/notes", (request, response) => {
+  response.json(notes);
+});
 
-app.get('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+app.get("/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
   if (note) {
-    response.json(note)
+    response.json(note);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-app.delete('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete("/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
